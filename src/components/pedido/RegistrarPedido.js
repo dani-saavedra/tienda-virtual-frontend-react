e3390b0e5d74380eb8115bba1a9607771b1a1958
Daniel Saavedra <danielink28@gmail.com>
2021-12-09 21:58:45 -0500

    Logica para manejo de la tabla de agregar producto, actualizar y eliminar

--- src/components/pedido/RegistrarPedido.js
+++ src/components/pedido/RegistrarPedido.js
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import obtenerClientes from "../../api/Cliente.api"
 import productoAPI from "../../api/Producto.api"
+import NoExiste from "../NoExiste";
 
 
 const RegistraPedido = () => {
 
-    let cantidadSeleccionada, productoSeleccionado;
+    let cantidadSeleccionada, productoSeleccionado, clienteSeleccionado;
 
     const [productosCarrito, setProductoCarrito] = useState([])
 
     const [clientes, setClientes] = useState([])
 
     const [productos, setProductos] = useState([])
+
+    const [totalCompra, setTotalCompra] = useState(0)
 
     useEffect(() => {
         obtenerClientes()
@@ -28,7 +31,15 @@ const RegistraPedido = () => {
             .catch((error) => {
                 console.log(error)
             })
-    }, [productosCarrito])
+    }, [])
+
+    const limpie = () => {
+        //location.reload();
+        setProductoCarrito([])
+        setTotalCompra(0)
+        clienteSeleccionado.value = ""
+
+    }
 
     const agregarProducto = () => {
         productoAPI.consultarProductoPorReferencia(productoSeleccionado.value)
@@ -36,28 +47,50 @@ const RegistraPedido = () => {
                 if (producto.cantidad < cantidadSeleccionada.value) {
                     alert("El producto no cuenta con esa cantidad disponible")
                 } else {
-                    let carrito = productosCarrito
-                    console.log(carrito)
-                    carrito.push({
-                        producto,
-                        cantidadSeleccionada: parseInt(cantidadSeleccionada.value),
-                        total: parseInt(cantidadSeleccionada.value) * producto.precio
-                    })
-                    setProductoCarrito(carrito);
+                    validarAgregarProducto(producto, cantidadSeleccionada.value)
                 }
             })
-
-
-
-
     }
-    console.log("voy a pintar")
-    console.log(productosCarrito)
+
+    const validarAgregarProducto = (nuevoProducto, cantidadSeleccionada) => {
+        let index = productosCarrito.map((e) => e.producto.referencia).indexOf(nuevoProducto.referencia);
+        if (index !== -1) {
+            //actualizar
+            let item = productosCarrito[index];
+            item.cantidadSeleccionada = item.cantidadSeleccionada + parseInt(cantidadSeleccionada)
+            let carrito = eliminarProduct(item.producto.referencia, item.total)
+            adicionarProductoLista(item.producto, item.cantidadSeleccionada, carrito, (totalCompra - item.total))
+        } else {
+            //agregar uno nuevo
+            adicionarProductoLista(nuevoProducto, cantidadSeleccionada, productosCarrito, totalCompra)
+        }
+    }
+
+    const adicionarProductoLista = (nuevoProducto, cantidadSeleccionada, items, total) => {
+        let carrito = [...items]
+        carrito.push({
+            producto: nuevoProducto,
+            cantidadSeleccionada: parseInt(cantidadSeleccionada),
+            total: parseInt(cantidadSeleccionada) * nuevoProducto.precio
+        })
+        setTotalCompra(total + (parseInt(cantidadSeleccionada) * nuevoProducto.precio))
+        setProductoCarrito(carrito);
+    }
+
+    const eliminarProduct = (referencia, totalItem) => {
+        let index = productosCarrito.map((e) => e.producto.referencia).indexOf(referencia)
+        let carrito = [...productosCarrito]
+        carrito.splice(index, 1)
+        setProductoCarrito(carrito)
+        setTotalCompra(totalCompra - totalItem)
+        return carrito
+    }
+
     return <div>
         <h1>Nuevo Pedido</h1>
         <label>Cliente </label>
-        <select id="cliente" className="form-select">
-            <option />
+        <select id="cliente" className="form-select" ref={cli => clienteSeleccionado = cli}>
+            <option value="" />
             {clientes.map((nombreCliente, index) => {
                 return <option key={index} value={nombreCliente}> {nombreCliente}</option>
             })}
@@ -86,6 +119,7 @@ const RegistraPedido = () => {
                             <th>Cantidad</th>
                             <th>Precio Unitario</th>
                             <th>Precio Total</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -95,11 +129,21 @@ const RegistraPedido = () => {
                                 <td>{item.cantidadSeleccionada}</td>
                                 <td>{item.producto.precio}</td>
                                 <td>{item.total}</td>
+                                <td><button onClick={() => eliminarProduct(item.producto.referencia, item.total)}>Eliminar Producto</button></td>
                             </tr>
                         })}
-
                     </tbody>
+                    <tfoot>
+                        <tr>
+                            <th colSpan="3" >Total</th>
+                            <th>{totalCompra}</th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
                 </table>
+                <div>
+                    <button onClick={limpie}>Limpiar</button>
+                </div>
             </form>
         </div>
     </div>
